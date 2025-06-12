3689fec2adabc6b13ab3d8035b40e3cdc2491c41
Vikas Solankey <159683543+vikassolankey@users.noreply.github.com>
2025-06-12 11:09:30 +0530

    Delete Day1_ProgrammingElements/Level1PracticePrograms directory

--- Day1_ProgrammingElements/Level1PracticePrograms/findAge.java
+++
@@ -1,9 +0,0 @@
-public class findAge {
-    public static void main(String[] args) {
-        int harryBirthYear = 2000;
-        int currentYear = 2024;
-        int currentAge= currentYear-harryBirthYear; 
-        System.out.println("Harry's age in 2024 is "+ currentAge);
-    }
-    
-}
