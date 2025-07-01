40a60fd57ec660e107655ad4f893c6400b133d6b
vikassolankey <v.ssolankig@gmail.com>
2025-07-01 21:30:17 +0530

    [Vikas] Add: Single Inheritance Problems

---
+++ Inheritance/SingleInheritance/LibraryManagement.java
@@ -0,0 +1,41 @@
+
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Author Bio: " + bio);
+    }
+}
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+        
+        Author author = new Author("The Java Handbook", 2022, "John Doe", "Experienced Java Developer and Author.");
+        author.displayInfo();
+    }
+}
---
+++ Inheritance/SingleInheritance/SmartHome.java
@@ -0,0 +1,38 @@
+
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+public class SmartHome {
+    public static void main(String[] args) {
+
+        Thermostat thermostat = new Thermostat("TH-101", "Online", 22.5);
+        thermostat.displayStatus();
+    }
+}
